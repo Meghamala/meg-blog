34c0ff60bfd12d7bd6eda493f7967ca86f633b4d
Meghamala <meghamala1996@gmail.com>
2024-03-19 16:05:27 -0700

    nav bar for all components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './NavBar';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ListPage from './pages/ListPage';
@@ -10,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
     <div className="App">
-      <h1>My Blog</h1>
+      <NavBar />
       <div id="page-body">
         <Routes>
         <Route path="/" element={<HomePage/>} />
