a9a0380afadfae249f82cffd5403302967f4d1cd
Meghamala <meghamala1996@gmail.com>
2024-03-20 18:06:09 -0700

    initial firebase auth config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGbfGHMA_QksGU2iTCukWh0HFOCfrfmIw",
+  authDomain: "meg-react-project.firebaseapp.com",
+  projectId: "meg-react-project",
+  storageBucket: "meg-react-project.appspot.com",
+  messagingSenderId: "589785973587",
+  appId: "1:589785973587:web:3c661b5f108f4331f017a6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
