b181ae6d65e91b79e0c013616c48fe831247d366
Meghamala <meghamala1996@gmail.com>
2024-03-23 13:45:57 -0700

    adding reqs with auth token

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -14,7 +14,11 @@ const ArticlePage = () => {
     
     useEffect( () =>{
         const loadArticleInfo = async() => {
-            const response = await axios.get(`/api/articles/${articleId}`);
+            const token = user && await user.getIdToken();
+            const headers = token ? {authToken: token}: {};
+            const response = await axios.get(`/api/articles/${articleId}`, { headers 
+            });
+
             const newArticleInfo = response.data;
             setArticleInfo(newArticleInfo);
         }
@@ -24,7 +28,9 @@ const ArticlePage = () => {
     const article = articles.find(article => article.name === articleId)
 
     const addUpvote = async () => {
-        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const token = user && await user.getIdToken();
+        const headers = token ? {authToken: token}: {};
+        const response = await axios.put(`/api/articles/${articleId}/upvote`, null, {headers});
         const updatedArticle = response.data;
         setArticleInfo(updatedArticle);
     }
